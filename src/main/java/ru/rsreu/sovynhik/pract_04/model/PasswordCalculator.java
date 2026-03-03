dacd6d5eb29509d7b4c711c4bafee8fbdfc61677
Daniel Savushkin <savushckindaniil@yandex.ru>
2026-03-03 13:04:18 +0300

    refactor: логика разнесена по классам
    - для UI выбрана новая тема

---
+++ src/main/java/ru/rsreu/sovynhik/pract_04/model/PasswordCalculator.java
@@ -0,0 +1,34 @@
+package ru.rsreu.sovynhik.pract_04.model;
+
+public class PasswordCalculator {
+    private final double P;          // вероятность подбора
+    private final double V;          // скорость перебора (паролей/минута)
+    private final int T;             // срок действия (дней)
+    private final int alphabetSize;  // мощность алфавита
+
+    private long S_star;
+    private int minLength;
+
+    public PasswordCalculator(double P, double V, int T, int alphabetSize) {
+        this.P = P;
+        this.V = V;
+        this.T = T;
+        this.alphabetSize = alphabetSize;
+        calculate();
+    }
+
+    private void calculate() {
+        long Tminutes = T * 24L * 60L;
+        double totalAttempts = V * Tminutes;
+        S_star = (long) Math.ceil(totalAttempts / P);
+        minLength = (int) Math.ceil(Math.log(S_star) / Math.log(alphabetSize));
+    }
+
+    public long getS_star() {
+        return S_star;
+    }
+
+    public int getMinLength() {
+        return minLength;
+    }
+}
