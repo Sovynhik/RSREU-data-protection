78b1ed170d752ad9493eccbcee98c7773979fe50
Daniel Savushkin <savushckindaniil@yandex.ru>
2026-03-27 01:37:31 +0300

    chore: update build.gradle.kts

--- 04-practice/build.gradle.kts
+++ 04-practice/build.gradle.kts
@@ -11,10 +11,4 @@ repositories {
 
 dependencies {
     implementation("com.formdev:flatlaf:3.5")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
